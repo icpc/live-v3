bda929c8b4b98265f788cc31f44aaa94a176d65b
masha237 <zhogowamasha@gmail.com>
2022-03-28 16:08:30 +0300

    Added statistics

--- frontend/src/components/organisms/widgets/Statistics.js
+++ frontend/src/components/organisms/widgets/Statistics.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
-import { VERDICT_NOK, VERDICT_OK, VERDICT_UNKNOWN } from "../../../config";
+import { CELL_FONT_FAMILY, VERDICT_NOK, VERDICT_OK, VERDICT_UNKNOWN } from "../../../config";
+import { ProblemCell } from "../../atoms/ContestCells";
+import { Cell } from "../../atoms/Cell";
 
 const WidgetWrap = styled.div`
   width: 100%;
@@ -17,6 +19,7 @@ const Title = styled.div`
   color: white;
   font-size: 30px;
   text-align: center;
+  font-family: ${CELL_FONT_FAMILY}
 `;
 
 const Table = styled.div`
@@ -37,8 +40,9 @@ const Submissions = styled.div`
   color: white;
 `;
 
-const OK = styled.div.attrs(({ width }) => ({ style: { width: width } }))`
+const OK = styled(Cell).attrs(({ width }) => ({ style: { width: width } }))`
   background: ${VERDICT_OK};
+  transition: width linear 200ms;
 
   height: 100%;
   font-size: 18pt;
@@ -47,8 +51,10 @@ const OK = styled.div.attrs(({ width }) => ({ style: { width: width } }))`
   text-align: center;
 
 `;
-const Progress = styled.div.attrs(({ width }) => ({ style: { width: width } }))`
+const Progress = styled(Cell).attrs(({ width }) => ({ style: { width: width } }))`
   background-color: ${VERDICT_UNKNOWN};
+
+  transition: width linear 200ms;
   overflow: hidden;
   float: left;
   color: white;
@@ -57,8 +63,10 @@ const Progress = styled.div.attrs(({ width }) => ({ style: { width: width } }))`
   height: 100%;
   text-align: center;
 `;
-const Wrong = styled.div.attrs(({ width }) => ({ style: { width: width } }))`
+const Wrong = styled(Cell).attrs(({ width }) => ({ style: { width: width } }))`
   background-color: ${VERDICT_NOK};
+
+  transition: width linear 200ms;
   overflow: hidden;
   float: left;
 
@@ -67,31 +75,10 @@ const Wrong = styled.div.attrs(({ width }) => ({ style: { width: width } }))`
   text-align: center;
 `;
 
-
-const OKText = styled.div`
-  height: 100%; 
+const StatisticsText = styled.div`
   margin: 0 0.1rem 0 0.1rem;
   text-align: center;
-`;
-
-const WAText = styled.div`
-  height: 100%; 
-  margin: 0 0.1rem 0 0.1rem;
-  text-align: center;
-`;
-
-const ProcessText = styled.div`
-  height: 100%; 
-  margin: 0 0.1rem 0 0.1rem;
-  text-align: center;
-`;
-
-const NameProblem = styled.div`
-  color: white;
-  height: 100%;
-  font-size: 18pt;
-  text-align: center;
-  width: 3rem;
+  font-family: ${CELL_FONT_FAMILY}
 `;
 
 const ProblemRow = styled.div.attrs(({ height }) => ({ style: { height: height } }))`
@@ -108,20 +95,20 @@ export const Advertisement = () => {
         <Table>
             {statistics?.map(({ success, wrong, pending }, index) =>
                 <ProblemRow key={index} height = {1 / statistics.length * 100 + "%"}>
-                    <NameProblem>{tasks[index].name}</NameProblem>
+                    <ProblemCell probData={tasks[index]} width={"35px"}/>
                     <Submissions>
                         <OK width={success / count * 100 + "%"}>
-                            <OKText>{success}</OKText>
+                            <StatisticsText>{success}</StatisticsText>
                         </OK>
                         <Progress width={pending / count * 100 + "%"}>
-                            <ProcessText>
+                            <StatisticsText>
                                 {pending}
-                            </ProcessText>
+                            </StatisticsText>
                         </Progress>
                         <Wrong width={wrong / count * 100 + "%"}>
-                            <WAText>
+                            <StatisticsText>
                                 {wrong}
-                            </WAText>
+                            </StatisticsText>
                         </Wrong>
                     </Submissions>
                 </ProblemRow>
