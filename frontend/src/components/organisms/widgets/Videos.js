4a250fe9ffc074791636614cd78e6d3b43fe7d46
masha237 <zhogowamasha@gmail.com>
2022-03-29 14:15:46 +0300

    add Videos widget

--- frontend/src/components/organisms/widgets/Videos.js
+++ frontend/src/components/organisms/widgets/Videos.js
@@ -1,12 +1,8 @@
 import React, { useState } from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import PropTypes from "prop-types";
-import { PICTURES_APPEAR_TIME, VERDICT_NOK } from "../../../config";
-import { pushLog } from "../../../redux/debug";
-import { useDispatch } from "react-redux";
 
-
-const VideoContainerWrap = styled.div`
+const VideosContainerWrap = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
@@ -16,43 +12,32 @@ const VideoContainerWrap = styled.div`
 `;
 
 
-const VideoContainer = styled.div`
+const VideosContainer = styled.div`
   display: grid;
   justify-content: center;
   text-align: center;
 `;
 
-
-const VideoCaptionWrap = styled.div`
-  background-color: ${VERDICT_NOK};
-  font-size: 24pt;
-  font-family: Passageway, serif;
-  color: white;
-  align-self: stretch;
-`;
-
-const VideoImgWrap = styled.div`
+const VideosWrap = styled.div`
   background-color: white;
 `;
 
-export const Video = ({ widgetData }) => {
-    const [isLoaded, setIsLoaded] = useState(false);
-    const dispatch = useDispatch();
-    return <VideoContainerWrap
+export const Videos = ({ widgetData }) => {
+    const [isLoaded] = useState(false);
+    return <VideosContainerWrap
         show={isLoaded}
     >
-        <VideoContainer>
-            <VideoWrap>
-              <video width={100%} height={100%} src={widgetData.video.url}>
-                  {widgetData.picture.name}
-              </video>
-            </VideoWrap>
-            <VideoCaptionWrap>{widgetData.picture.name}</VideoCaptionWrap>
-        </VideoContainer>
-    </VideoContainerWrap>;
+        <VideosContainer>
+            <VideosWrap>
+                <video width={"100%"} height={"100%"} src={widgetData.videos.url}>
+                    widgetData.videos.name
+                </video>
+            </VideosWrap>
+        </VideosContainer>
+    </VideosContainerWrap>;
 };
 
-Pictures.propTypes = {
+Videos.propTypes = {
     widgetData: PropTypes.shape({
         picture: PropTypes.shape({
             url: PropTypes.string.isRequired,
@@ -61,4 +46,4 @@ Pictures.propTypes = {
     }),
 };
 
-export default Pictures;
+export default Videos;
