be3e09ddcd48135ed4b29aa6751d340d07bf6b2e
Azat Ismagilov <azatismagilov00@gmail.com>
2022-03-30 17:25:36 +0300

    WebApi scoreboard support

--- backend/src/main/kotlin/org/icpclive/adminapi/Routing.kt
+++ backend/src/main/kotlin/org/icpclive/adminapi/Routing.kt
@@ -5,6 +5,7 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import kotlinx.html.*
 import org.icpclive.adminapi.advertisement.configureAdvertisementApi
+import org.icpclive.adminapi.scoreboard.configureScoreboardApi
 
 private lateinit var topLevelLinks: List<Pair<String, String>>
 
@@ -30,10 +31,12 @@ suspend inline fun ApplicationCall.catchAdminApiAction(block: ApplicationCall.()
 fun Application.configureAdminApiRouting() {
     routing {
         val advertisementUrls =
-            configureAdvertisementApi(environment.config.property("live.presets.advertisements").getString())
+                configureAdvertisementApi(environment.config.property("live.presets.advertisements").getString())
+        val scoreboardUrls = configureScoreboardApi()
 
         topLevelLinks = listOf(
-            advertisementUrls.mainPage to "Advertisement",
+                advertisementUrls.mainPage to "Advertisement",
+                scoreboardUrls.mainPage to "Scoreboard",
         )
         get("/adminapi") {
             call.respondRedirect(topLevelLinks[0].first)
