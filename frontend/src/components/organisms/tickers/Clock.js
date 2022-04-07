725a718c882649c373e64cd92839217bbe9aa8a0
Maxim <me@alzhanov.ru>
2022-04-08 02:11:07 +0300

    frontend: make clock messages follow emulation speed

--- frontend/src/components/organisms/tickers/Clock.js
+++ frontend/src/components/organisms/tickers/Clock.js
@@ -6,12 +6,13 @@ import { TextWrap } from "./Text";
 export const Clock = ({ tickerSettings }) => {
     const contestInfo = useSelector((state) => state.contestInfo.info);
     const getStatus = useCallback(() => {
-        const milliseconds = DateTime.fromMillis(contestInfo.startTimeUnixMs).diffNow().negate().milliseconds;
+        const milliseconds = DateTime.fromMillis(contestInfo.startTimeUnixMs).diffNow().negate().milliseconds *
+            (contestInfo.emulationSpeed ?? 0);
         if(milliseconds < 0)
             return "BEFORE";
         if(milliseconds >= contestInfo.contestLengthMs)
             return "OVER";
-        return DateTime.fromMillis(milliseconds * (contestInfo.emulationSpeed ?? 0)).toFormat("h:mm:ss");
+        return DateTime.fromMillis(milliseconds).toFormat("h:mm:ss");
     }, [contestInfo]);
     const [status, setStatus] = useState(getStatus());
     useEffect(() => {
