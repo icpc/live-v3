3bb9b6ed3649862391d78fd68ac03e5f8bc7b33b
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2022-04-16 23:22:29 +0300

    Yandex parser stub to show to Nikita

--- backend/src/main/kotlin/org/icpclive/cds/yandex/YandexEventLoader.kt
+++ backend/src/main/kotlin/org/icpclive/cds/yandex/YandexEventLoader.kt
@@ -1,12 +1,27 @@
 package org.icpclive.cds.yandex
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
+import org.icpclive.api.ContestInfo
+import org.icpclive.api.RunInfo
 import org.icpclive.cds.yandex.api.Participant
 import org.icpclive.config.Config
+import org.icpclive.data.DataBus
+import org.icpclive.service.EmulationService
 import org.icpclive.service.RegularLoaderService
+import org.icpclive.service.RunsBufferService
+import org.icpclive.service.launchICPCServices
 import org.icpclive.utils.OAuthAuth
 import org.icpclive.utils.getLogger
+import org.icpclive.utils.guessDatetimeFormat
+import org.icpclive.utils.humanReadable
+import java.util.*
+import kotlin.time.Duration.Companion.seconds
 
 class YandexEventLoader  {
     val apiKey: String
@@ -32,6 +47,42 @@ class YandexEventLoader  {
             override val auth = OAuthAuth(apiKey)
             override fun processLoaded(data: String) = data
         }
+        val properties: Properties = Config.loadProperties("events")
+        val emulationSpeedProp: String? = properties.getProperty("emulation.speed")
+        val contestInfo : ContestInfo = TODO()
+        val contestInfoFlow = MutableStateFlow(contestInfo).also { DataBus.contestInfoUpdates.complete(it) }
+
+        if (emulationSpeedProp != null) {
+            coroutineScope {
+                val emulationSpeed = emulationSpeedProp.toDouble()
+                val emulationStartTime = guessDatetimeFormat(properties.getProperty("emulation.startTime"))
+                log.info("Running in emulation mode with speed x${emulationSpeed} and startTime = ${emulationStartTime.humanReadable}")
+                val rawRunsFlow = MutableSharedFlow<RunInfo>(
+                    extraBufferCapacity = 100000,
+                    onBufferOverflow = BufferOverflow.SUSPEND
+                )
+                launch {
+                    EmulationService(
+                        emulationStartTime,
+                        emulationSpeed,
+                        TODO(),
+                        contestInfo,
+                        contestInfoFlow,
+                        rawRunsFlow
+                    ).run()
+                }
+                launchICPCServices(contestInfo.problems.size, rawRunsFlow)
+            }
+        } else {
+            coroutineScope {
+                val rawRunsFlow = MutableSharedFlow<RunInfo>(
+                    extraBufferCapacity = 100000,
+                    onBufferOverflow = BufferOverflow.SUSPEND
+                )
+                launchICPCServices(contestInfo.problems.size, rawRunsFlow)
+            }
+        }
+
     }
 
     companion object {
