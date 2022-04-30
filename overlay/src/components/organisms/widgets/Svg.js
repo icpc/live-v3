575d875d2741244069696cf58d8dd5ed3b445065
Azat Ismagilov <azatismagilov00@gmail.com>
2022-05-01 01:38:40 +0300

    Some changes in titles

--- overlay/src/components/organisms/widgets/Svg.js
+++ overlay/src/components/organisms/widgets/Svg.js
@@ -1,24 +1,18 @@
 import React from "react";
-import styled from "styled-components";
 import { SVG_APPEAR_TIME } from "../../../config";
-
-const SvgContainer = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
+import PropTypes from "prop-types";
 
 export const Svg = ({ widgetData }) => {
-    return <SvgContainer>
-        <img
-            src={widgetData.content}
-            alt="None"
-        />
-    </SvgContainer>;
+    return <img
+        src={widgetData.content}
+        alt="None"
+    />;
+};
+
+Svg.propTypes = {
+    widgetData: PropTypes.shape({
+        content: PropTypes.string.isRequired
+    })
 };
 
 Svg.overrideTimeout = SVG_APPEAR_TIME;
