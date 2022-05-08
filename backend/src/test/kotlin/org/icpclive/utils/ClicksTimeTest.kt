2d782b0946a646f68b5cd1d3fe9a20eb0210e37d
kbats183 <kbats@bk.ru>
2022-05-08 18:52:41 +0300

    WF2. Add cds Duration serialisation

--- backend/src/test/kotlin/org/icpclive/utils/ClicksTimeTest.kt
+++ backend/src/test/kotlin/org/icpclive/utils/ClicksTimeTest.kt
@@ -1,9 +1,20 @@
 package org.icpclive.utils
 
-import org.junit.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.icpclive.utils.ClicksTime.formatIso
+import org.icpclive.utils.ClicksTime.parseRelativeTime
+import org.junit.Assert
+import org.junit.Test
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.math.roundToLong
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 import kotlin.time.DurationUnit
 
 class ClicksTimeTest {
@@ -53,6 +64,7 @@ class ClicksTimeTest {
         arrayOf("-12:34", "-12:34")
     )
     var signs = arrayOf(arrayOf("", 1), arrayOf("+", 1), arrayOf("-", -1))
+
     @Test
     fun parseTimeTest() {
         for (YY in years) for (MM in months) for (DD in days) for (hh in hours) for (mm in minutes) for (ss in seconds) for (zz in zones) {
@@ -75,53 +87,78 @@ class ClicksTimeTest {
     }
 
     var relHours = arrayOf(
-        arrayOf(null, 0),
-        arrayOf("00", 0),
-        arrayOf("0", 0),
-        arrayOf("1", 1),
-        arrayOf("8", 8),
-        arrayOf("08", 8),
-        arrayOf("23", 23)
+        null to 0,
+        "00" to 0,
+        "0" to 0,
+        "1" to 1,
+        "8" to 8,
+        "08" to 8,
+        "23" to 23
     )
     var relMinutes = arrayOf(
-        arrayOf<Any?>(null, 0),
-        arrayOf("00", 0),
-        arrayOf("0", 0),
-        arrayOf("8", 8),
-        arrayOf("08", 8),
-        arrayOf("59", 59)
+        null to 0,
+        "00" to 0,
+        "0" to 0,
+        "8" to 8,
+        "08" to 8,
+        "59" to 59
     )
     var relSeconds = arrayOf(
-        arrayOf("00", 0.0),
-        arrayOf("0", 0.0),
-        arrayOf("8", 8.0),
-        arrayOf("08", 8.0),
-        arrayOf("59", 59.0),
-        arrayOf("1.2", 1.2),
-        arrayOf("1.23", 1.23),
-        arrayOf("1.234", 1.234),
-        arrayOf("1.23456", 1.235)
+        "00" to 0.0,
+        "0" to 0.0,
+        "8" to 8.0,
+        "08" to 8.0,
+        "59" to 59.0,
+        "1.2" to 1.2,
+        "1.23" to 1.23,
+        "1.234" to 1.234,
+        "1.23456" to 1.235
     )
-    var relSigns = arrayOf(arrayOf("", 1), arrayOf("+", 1), arrayOf("-", -1))
+    var relSigns = arrayOf("" to 1, "+" to 1, "-" to -1)
+
     @Test
     fun parseRelativeTimeTest() {
         for (pp in relSigns) for (hh in relHours) for (mm in relMinutes) for (ss in relSeconds) {
-            if (mm[0] == null && hh[0] != null) continue
+            if (mm.first == null && hh.first != null) continue
             val testRelTime = String.format(
                 "%s%s%s%s",
-                pp[0], if (hh[0] != null) "" + hh[0] + ":" else "", if (mm[0] != null) "" + mm[0] + ":" else "", ss[0]
+                pp.first,
+                if (hh.first != null) "" + hh.first + ":" else "",
+                if (mm.first != null) "" + mm.first + ":" else "",
+                ss.first
             )
-            val t = pp[1] as Int * (60.0 * 60.0 * hh[1] as Int + 60.0 * mm[1] as Int + ss[1] as Double)
+            val t = pp.second * (60.0 * 60.0 * hh.second + 60.0 * mm.second + ss.second)
             val expect = Math.round(t * 1000.0)
-            val s = ss[1] as Double
+            val s = ss.second
             val `is` = s.toInt()
             val ns = Math.round(1e9 * (s - `is`)).toInt()
             val isoInstant = String.format(
                 "%s%02d:%02d:%02d.%09d",
-                if (pp[1] as Int == 1) "+" else "-", hh[1], mm[1], `is`, ns
+                if (pp.second == 1) "+" else "-", hh.second, mm.second, `is`, ns
             )
-            val result: Long = ClicksTime.parseRelativeTime(testRelTime).toDouble(DurationUnit.MILLISECONDS).roundToLong()
+            val result: Long = parseRelativeTime(testRelTime).toDouble(DurationUnit.MILLISECONDS).roundToLong()
             Assert.assertEquals("$testRelTime vs $isoInstant", expect, result)
+        }
+    }
+
+    @Test
+    fun durationJsonSerialisation() {
+        @Serializable
+        data class ObjectWithDuration(
+            @Serializable(with = ClicksTime.DurationSerializer::class)
+            val dur: Duration
+        )
+
+        for (pp in relSigns) for (hh in relHours) for (mm in relMinutes) for (ss in relSeconds) {
+            hh.first ?: continue
+            mm.first ?: continue
+            val duration = (hh.second.hours + mm.second.minutes + ss.second.seconds) * pp.second
+            val durationString = formatIso(duration)
+            parseRelativeTime(durationString)
+            val obj = ObjectWithDuration(duration)
+            val encodedString = Json.encodeToString(obj)
+            Assert.assertEquals("{\"dur\":\"$durationString\"}", encodedString)
+            Assert.assertEquals(obj, Json.decodeFromString<ObjectWithDuration>(encodedString))
         }
     }
 }
