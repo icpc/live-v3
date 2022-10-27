831cf48249a68d6f1fafd7f4a48c1d6cb3539890
Azat Ismagilov <azatismagilov00@gmail.com>
2022-10-27 18:43:54 +0300

    Support for external social function

---
+++ backend/src/main/kotlin/org/icpclive/admin/Social.kt
@@ -0,0 +1,30 @@
+package org.icpclive.admin
+
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import kotlinx.coroutines.flow.MutableSharedFlow
+import org.icpclive.api.ChatMessage
+import org.icpclive.api.SocialEvent
+import org.icpclive.util.completeOrThrow
+import org.icpclive.data.DataBus
+import org.icpclive.util.sendJsonFlow
+
+fun Route.setupSocial() {
+    val socialFlow = MutableSharedFlow<SocialEvent>(extraBufferCapacity = 10000)
+    DataBus.socialEvents.completeOrThrow(socialFlow)
+
+    suspend fun processMessage(message: ChatMessage): ChatMessage {
+        val newTeamIds =
+            message.teamIds + getHashtags().entries.filter { message.rawText.contains(it.key) }.map { it.value }
+        return message.copy(teamIds = newTeamIds.distinct())
+    }
+
+    webSocket { sendJsonFlow(DataBus.socialEvents.await()) }
+
+    post {
+        call.adminApiAction {
+            socialFlow.emit(processMessage(call.safeReceive()))
+        }
+    }
+}
