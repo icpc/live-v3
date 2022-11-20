dba6d011f3e18b13ad41c6e148ea0812fe4f84e8
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2022-11-20 12:26:21 +0100

    More idiomatic cancellation support in fileChangesFlow

--- common/src/main/kotlin/org/icpclive/util/FileChangesWatcher.kt
+++ common/src/main/kotlin/org/icpclive/util/FileChangesWatcher.kt
@@ -1,10 +1,8 @@
 package org.icpclive.util
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import java.nio.file.Path
 import java.nio.file.StandardWatchEventKinds
 import java.nio.file.WatchEvent
@@ -14,29 +12,27 @@ import kotlin.io.path.listDirectoryEntries
 
 fun directoryChangesFlow(path: Path) =
     flow {
-        while (true) {
+        while (currentCoroutineContext().isActive) {
             path.fileSystem.newWatchService().use { watcher ->
                 path.register(
                     watcher,
                     arrayOf(StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY)
                 )
                 path.listDirectoryEntries().forEach { emit(it.fileName) }
-                while (true) {
-                    val key = watcher.poll(100, TimeUnit.MILLISECONDS)
-                    if (key != null) {
-                        for (event in key.pollEvents()) {
-                            val kind = event.kind()
-                            if (kind === StandardWatchEventKinds.OVERFLOW) {
-                                path.listDirectoryEntries().forEach { emit(it) }
-                                continue
-                            }
-                            @Suppress("UNCHECKED_CAST")
-                            emit((event as WatchEvent<Path>).context())
+                while (currentCoroutineContext().isActive) {
+                    val key = watcher.poll(100, TimeUnit.MILLISECONDS) ?: continue
+                    for (event in key.pollEvents()) {
+                        val kind = event.kind()
+                        if (kind === StandardWatchEventKinds.OVERFLOW) {
+                            path.listDirectoryEntries().forEach { emit(it) }
+                            continue
                         }
+                        @Suppress("UNCHECKED_CAST")
+                        emit((event as WatchEvent<Path>).context())
+                    }
 
-                        if (!key.reset()) {
-                            break
-                        }
+                    if (!key.reset()) {
+                        break
                     }
                 }
             }
