a42cc809fea1af82572a6da3b81884972f79a93f
Kostya Bats <kbats@bk.ru>
2022-12-07 02:21:50 +0300

    Skip team in automode teamview is there are no media

--- src/backend/src/main/kotlin/org/icpclive/controllers/TeamViewController.kt
+++ src/backend/src/main/kotlin/org/icpclive/controllers/TeamViewController.kt
@@ -12,8 +12,7 @@ class TeamViewController(manager: Manager<TeamViewWidget>, val position: TeamVie
     override suspend fun onDelete(id: Int) {}
 
     override suspend fun constructWidget(settings: ExternalTeamViewSettings): TeamViewWidget {
-        val teamInfo = DataBus.contestInfoFlow.await().first().teams.find { it.id == settings.teamId }
-        val content = settings.mediaTypes.mapNotNull { teamInfo?.medias?.get(it) }.toMutableList()
+        val (teamInfo, content) = getTeamInfoAndMediaContent(settings)
         if (settings.showTaskStatus) {
             settings.teamId?.let { teamId -> content.add(MediaType.TaskStatus(teamId)) }
         }
@@ -23,10 +22,20 @@ class TeamViewController(manager: Manager<TeamViewWidget>, val position: TeamVie
         return TeamViewWidget(OverlayTeamViewSettings(content, position))
     }
 
+    private suspend fun getTeamInfoAndMediaContent(settings: ExternalTeamViewSettings): Pair<TeamInfo?, MutableList<MediaType>> {
+        val teamInfo = DataBus.contestInfoFlow.await().first().teams.find { it.id == settings.teamId }
+        val content = settings.mediaTypes.mapNotNull { teamInfo?.medias?.get(it) }.toMutableList()
+        return teamInfo to content
+    }
+
     override suspend fun createWidgetAndShow(settings: ExternalTeamViewSettings) {
         if (settings.teamId == null) {
             launchWhileWidgetShown {
                 DataBus.teamSpotlightFlow.await().collect {
+                    val (_, content) = getTeamInfoAndMediaContent(settings)
+                    if (content.isEmpty()) {
+                        return@collect
+                    }
                     val staticSettings = settings.copy(teamId = it.teamId)
                     super.createWidgetAndShow(staticSettings)
                     delay(30.seconds)
