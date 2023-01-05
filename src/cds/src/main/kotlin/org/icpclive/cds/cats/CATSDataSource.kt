00df1d52573827db92d0f7e9c2086398f389f28d
Alexey Grunskii <you@example.com>
2023-01-05 10:40:09 +0300

    Add cats config folder

--- src/cds/src/main/kotlin/org/icpclive/cds/cats/CATSDataSource.kt
+++ src/cds/src/main/kotlin/org/icpclive/cds/cats/CATSDataSource.kt
@@ -3,7 +3,7 @@ package org.icpclive.cds.cats
 import org.icpclive.cds.ContestParseResult
 import org.icpclive.cds.FullReloadContestDataSource
 import org.icpclive.cds.common.ClientAuth
-import org.icpclive.cds.common.xmlLoaderService
+import org.icpclive.cds.common.stringLoaderService
 import org.icpclive.util.getCredentials
 import java.util.*
 import kotlin.time.Duration.Companion.seconds
@@ -11,11 +11,16 @@ import kotlin.time.Duration.Companion.seconds
 class CATSDataSource(val properties: Properties, creds: Map<String, String>): FullReloadContestDataSource(5.seconds) {
     private val login = properties.getCredentials("login", creds)
     private val password = properties.getCredentials("password", creds)
-    private val dataLoader = xmlLoaderService(login?.let { ClientAuth.Basic(login, password!!) }) {
+    private val dataLoader = stringLoaderService(login?.let { ClientAuth.Basic(login, password!!) }) {
         properties.getProperty("url")
+    }
+
+    init {
+        println("CATSDataSource initialized")
     }
 
     override suspend fun loadOnce(): ContestParseResult {
         TODO("Not yet implemented")
     }
+
 }
