6383e82f7d699b882de2f8900665ebb9126379b5
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2023-01-21 13:39:18 +0100

    Cats fixes

--- src/cds/src/main/kotlin/org/icpclive/cds/cats/CATSDataSource.kt
+++ src/cds/src/main/kotlin/org/icpclive/cds/cats/CATSDataSource.kt
@@ -1,39 +1,65 @@
 package org.icpclive.cds.cats
 
 import kotlinx.coroutines.*
+import kotlinx.datetime.*
 import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toInstant
-import kotlinx.datetime.toKotlinLocalDateTime
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import org.icpclive.api.*
 import org.icpclive.cds.ContestParseResult
 import org.icpclive.cds.FullReloadContestDataSource
 import org.icpclive.cds.common.jsonLoaderService
 import org.icpclive.util.getCredentials
 import org.icpclive.util.getLogger
-import java.awt.Color
-import java.time.LocalDateTime
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 import kotlin.time.Duration.Companion.seconds
 
+private object ContestTimeSerializer : KSerializer<LocalDateTime> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("InstantConstand", PrimitiveKind.STRING)
+    private val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
+
+    override fun serialize(encoder: Encoder, value: LocalDateTime) {
+        encoder.encodeString(formatter.format(value.toJavaLocalDateTime()))
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDateTime {
+        return java.time.LocalDateTime.parse(decoder.decodeString(), formatter).toKotlinLocalDateTime()
+    }
+}
+
+private object SubmissionTimeSerializer : KSerializer<Instant> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("InstantConstand", PrimitiveKind.STRING)
+    private val formatter = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmssZ")
+
+    override fun serialize(encoder: Encoder, value: Instant) {
+        encoder.encodeString(formatter.format(value.toJavaInstant()))
+    }
+
+    override fun deserialize(decoder: Decoder): Instant {
+        return ZonedDateTime.parse(decoder.decodeString(), formatter).toInstant().toKotlinInstant()
+    }
+}
 
 class CATSDataSource(val properties: Properties, creds: Map<String, String>) : FullReloadContestDataSource(5.seconds) {
     private val login = properties.getCredentials("login", creds)
     private val password = properties.getCredentials("password", creds)
     private val url = properties.getProperty("url")
-    private val timezone: String = properties.getProperty("timezone")
+    private val timezone = TimeZone.of(properties.getProperty("timezone") ?: throw IllegalStateException("Cats requires timezone property to be set"))
     private val resultType: ContestResultType = ContestResultType.valueOf(
         properties.getProperty("standings.resultType")?.toString()?.uppercase() ?: "ICPC"
     )
+    private val cid = properties.getProperty("cid")
 
     private val logger = getLogger(CATSDataSource::class)
-
-    // :TODO I may send less count of quires to the server but I need to know when my sid is expired
-    private fun sid(): String = runBlocking {
-        async { authLoader.loadOnce().sid }.await()
-    }
+    private var sid: String? = null
 
     @Serializable
     data class Auth(val status: String, val sid: String, val cid: Long)
@@ -53,9 +79,12 @@ class CATSDataSource(val properties: Properties, creds: Map<String, String>) : F
     @Serializable
     data class Contest(
         val title: String,
-        val start_date: String,
-        val freeze_date: String,
-        val finish_date: String,
+        @Serializable(with = ContestTimeSerializer::class)
+        val start_date: LocalDateTime,
+        @Serializable(with = ContestTimeSerializer::class)
+        val freeze_date: LocalDateTime,
+        @Serializable(with = ContestTimeSerializer::class)
+        val finish_date: LocalDateTime,
         val rules: String
     )
 
@@ -69,7 +98,8 @@ class CATSDataSource(val properties: Properties, creds: Map<String, String>) : F
         val state_text: String,
         val problem_id: Int,
         val team_id: Int,
-        val submit_time: String,
+        @Serializable(with = SubmissionTimeSerializer::class)
+        val submit_time: Instant,
         val points: Double = 0.0
     ) : Run()
 
@@ -95,23 +125,14 @@ class CATSDataSource(val properties: Properties, creds: Map<String, String>) : F
         val contest_start: Int
     ) : Run()
 
-    private val authLoader =
-        jsonLoaderService<Auth> { "$url/?f=login&login=$login&passwd=$password&json=1" }
-    private val problemsLoader =
-        jsonLoaderService<Problems> { "$url/problems?cid=${properties.getProperty("cid")}&sid=${sid()}&json=1" }
-    private val usersLoader =
-        jsonLoaderService<Users> { "$url/users?cid=${properties.getProperty("cid")}&sid=${sid()}&rows=1000&json=1" }
-    private val contestLoader =
-        jsonLoaderService<Contest> { "$url/contest_params?cid=${properties.getProperty("cid")}&sid=${sid()}&json=1" }
-    private val runsLoader =
-        jsonLoaderService<List<Run>> { "$url/console?cid=${properties.getProperty("cid")}&sid=${sid()}&rows=1000&json=1" }
-
-    private fun getDateTime(dateTime: String, formatter: DateTimeFormatter) = LocalDateTime
-        .parse(dateTime, formatter)
-        .toKotlinLocalDateTime()
-        .toInstant(TimeZone.of(timezone))
+    private val authLoader = jsonLoaderService<Auth> { "$url/?f=login&login=$login&passwd=$password&json=1" }
+    private val problemsLoader = jsonLoaderService<Problems> { "$url/problems?cid=$cid&sid=${sid!!}&json=1" }
+    private val usersLoader = jsonLoaderService<Users> { "$url/users?cid=$cid&sid=${sid!!}&rows=1000&json=1" }
+    private val contestLoader = jsonLoaderService<Contest> { "$url/contest_params?cid=$cid&sid=${sid!!}&json=1" }
+    private val runsLoader = jsonLoaderService<List<Run>> { "$url/console?cid=$cid&sid=${sid!!}&rows=1000&json=1" }
 
     override suspend fun loadOnce(): ContestParseResult {
+        sid = authLoader.loadOnce().sid
         return parseAndUpdateStandings(
             problemsLoader.loadOnce(),
             usersLoader.loadOnce(),
@@ -132,12 +153,12 @@ class CATSDataSource(val properties: Properties, creds: Map<String, String>) : F
                 ProblemInfo(
                     letter = problem.code,
                     name = problem.name,
-                    color = Color(0x000000),
+                    color = null,
                     id = problem.id,
                     ordinal = index,
                     minScore = if (resultType == ContestResultType.IOI) 0.0 else null,
                     maxScore = if (resultType == ContestResultType.IOI) problem.max_points else null,
-                    scoreMergeMode = if (resultType == ContestResultType.IOI) ScoreMergeMode.MAX_PER_GROUP else null
+                    scoreMergeMode = if (resultType == ContestResultType.IOI) ScoreMergeMode.MAX_TOTAL else null
                 )
             }
             .toList()
@@ -157,12 +178,9 @@ class CATSDataSource(val properties: Properties, creds: Map<String, String>) : F
                 )
             }.toList()
 
-        val runsFormatter = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss'+1000'")
-        val contestTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
-
-        val startTime = getDateTime(contest.start_date, contestTimeFormatter)
-        val contestLength = getDateTime(contest.finish_date, contestTimeFormatter) - startTime
-        val freezeTime = getDateTime(contest.freeze_date, contestTimeFormatter) - startTime
+        val startTime = contest.start_date.toInstant(timezone)
+        val contestLength = contest.finish_date.toInstant(timezone) - startTime
+        val freezeTime = contest.freeze_date.toInstant(timezone) - startTime
 
         val contestInfo = ContestInfo(
             status = ContestStatus.OVER,
@@ -176,36 +194,30 @@ class CATSDataSource(val properties: Properties, creds: Map<String, String>) : F
 
         val resultRuns = runs
             .asSequence()
-            .filterIsInstance(Submit::class.java)
-            .map { it: Run -> it as Submit }
-            .map { it: Submit ->
-                logger.debug(
-                    "SubmitTime: ${
-                        getDateTime(
-                            it.submit_time,
-                            runsFormatter
+            .filterIsInstance<Submit>()
+            .map {
+                val result = if (it.state_text.isNotEmpty()) {
+                    when (contestInfo.resultType) {
+                        ContestResultType.ICPC -> ICPCRunResult(
+                            isAccepted = ("OK" == it.state_text),
+                            isAddingPenalty = ("OK" != it.state_text && "CE" != it.state_text),
+                            isFirstToSolveRun = false,
+                            result = it.state_text
                         )
-                    }, time=${getDateTime(it.submit_time, runsFormatter) - startTime}"
-                )
+                        ContestResultType.IOI -> IOIRunResult(score = listOf(it.points))
+                    }
+                } else null
                 RunInfo(
                     id = it.id,
-                    if (contestInfo.resultType == ContestResultType.ICPC) ICPCRunResult(
-                        isAccepted = ("OK" == it.state_text),
-                        isAddingPenalty = ("OK" != it.state_text && "CE" != it.state_text),
-                        isFirstToSolveRun = false,
-                        result = it.state_text
-                    ) else {
-                        IOIRunResult(
-                            score = listOf(it.points)
-                        )
-                    },
+                    result = result,
                     problemId = it.problem_id,
                     teamId = it.team_id,
-                    percentage = if ("" == it.state_text) 0.0 else 1.0,
-                    time = getDateTime(it.submit_time, runsFormatter) - startTime
+                    percentage = if (result == null) 0.0 else 1.0,
+                    time = it.submit_time - startTime
                 )
             }
             .toList()
+            .sortedBy { it.time }
 
         return ContestParseResult(
             contestInfo,
