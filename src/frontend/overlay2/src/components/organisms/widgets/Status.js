e4fb643e772b71d211a47b4b1e4cdcab9934b1f2
Maxim <me@alzhanov.ru>
2023-04-04 15:26:31 +0500

    Remove overlay2 from repo and update peers

--- src/frontend/overlay2/src/components/organisms/widgets/Status.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export const Status = () => {
-    return <div>Status</div>;
-};
-export default Status;
--- src/frontend/overlay2/src/locations.js
+++
@@ -1,8 +0,0 @@
-export const LOCATIONS = {
-    TimelineWidget: {
-        positionX: 0,
-        positionY: 0,
-        sizeX: 0,
-        sizeY: 0
-    }
-};
