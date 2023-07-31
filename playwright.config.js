15c53c2a753df30a926d172226fe29531bd350f9
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2023-07-31 22:00:33 +0200

    Disable retry and decrease timeout for playwright

--- playwright.config.js
+++ playwright.config.js
@@ -17,7 +17,7 @@ module.exports = defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    retries: 0,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: "html",
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
