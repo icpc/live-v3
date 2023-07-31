1ed670e5e20b62bd3502f65d1fbd87e75021f2d4
Azat Ismagilov <azatismagilov00@gmail.com>
2023-08-01 01:00:58 +0200

    Capture stderr

--- playwright.config.js
+++ playwright.config.js
@@ -16,7 +16,7 @@ module.exports = defineConfig({
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
+    /* No retries */
     retries: 0,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: "html",
