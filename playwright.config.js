6286df0e631ef43ee07f01ebbb90ddb71f3c94a7
Azat Ismagilov <azatismagilov00@gmail.com>
2023-08-08 21:00:49 +0300

    Collect trace for failing tests

--- playwright.config.js
+++ playwright.config.js
@@ -25,8 +25,8 @@ module.exports = defineConfig({
         /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL: "http://127.0.0.1:8080",
 
-        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: "on-first-retry",
+        /* Collect trace for failing tests. See https://playwright.dev/docs/trace-viewer */
+        trace: "retain-on-failure",
     },
     /* Set timeout for each test to 2 minute */
     timeout: 2 * 60 * 1000,
