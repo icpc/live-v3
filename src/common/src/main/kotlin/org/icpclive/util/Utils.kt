075630c1da866bf5c3ab6b616c0a24c766c3c171
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2023-09-22 09:37:18 +0200

    Add custom message on ssl errors

--- src/common/src/main/kotlin/org/icpclive/util/Utils.kt
+++ src/common/src/main/kotlin/org/icpclive/util/Utils.kt
@@ -1,8 +1,6 @@
 package org.icpclive.util
 
-import kotlinx.coroutines.CancellationException
 import org.slf4j.LoggerFactory
-import java.util.*
 import kotlin.reflect.KClass
 
 inline fun <reified T> catchToNull(f: () -> T) = try {
@@ -12,16 +10,6 @@ inline fun <reified T> catchToNull(f: () -> T) = try {
 }
 
 fun getLogger(clazz: KClass<*>) = LoggerFactory.getLogger(clazz.java)!!
-
-fun Properties.getCredentials(key: String, creds: Map<String, String>) = getProperty(key)?.let {
-    val prefix = "\$creds."
-    if (it.startsWith(prefix)) {
-        val name = it.substring(prefix.length)
-        creds[name] ?: throw IllegalStateException("Credential $name not found")
-    } else {
-        it
-    }
-}?.takeIf { it.isNotEmpty() }
 
 class Enumerator<K> {
     val keys = mutableMapOf<K, Int>()
