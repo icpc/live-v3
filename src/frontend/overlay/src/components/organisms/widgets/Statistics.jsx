56ed86c1baee23fa32917f6bf34dee502126084a
Kostya Bats <kbats@bk.ru>
2023-10-08 16:21:43 +0300

    Add legend

--- src/frontend/overlay/src/components/organisms/widgets/Statistics.jsx
+++ src/frontend/overlay/src/components/organisms/widgets/Statistics.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import c from "../../../config";
 import { getTeamTaskColor } from "../../../utils/statusInfo";
 import { StackedBarsStatistics } from "../../molecules/statistics/StackedBarsStatistics";
+import { StatisticsLegends } from "../../molecules/statistics/StatisticsLegends";
 
 const StatisticsWrap = styled.div`
   width: 100%;
@@ -12,24 +13,29 @@ const StatisticsWrap = styled.div`
   background-color: ${c.CONTEST_COLOR};
   background-repeat: no-repeat;
   border-radius: ${c.GLOBAL_BORDER_RADIUS};
-  padding: 8px;
+  padding: 8px 16px;
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
-  gap: 7px;
+  gap: 8px;
 `;
 
 const StatisticsHeader = styled.div`
   font-size: 32px;
-  font-weight: 700;
   line-height: 44px;
   color: white;
   width: 100%;
   display: flex;
 `;
 
-const StatisticsHeaderTitle = styled.div`
+const StatisticsHeaderWrapper = styled.div`
   flex: 1 0 0;
+  display: flex;
+  gap: 16px;
+`
+
+const StatisticsHeaderTitle = styled.div`
+  font-weight: ${c.GLOBAL_DEFAULT_FONT_WEIGHT_BOLD};
 `;
 
 const StatisticsHeaderCaption = styled.div``;
@@ -38,13 +44,26 @@ const StatisticsHeaderCaption = styled.div``;
 const stackedBarsData = (resultType, tasks, statistics, count) => {
     if (!tasks || !statistics || !count) {
         return {
-            legend: [],
+            legends: [],
             bars: [],
         };
     }
 
+    const legends = [];
     const bars = [];
     if (resultType === "ICPC") {
+        legends.push({
+            caption: "solved",
+            color: c.VERDICT_OK2,
+        });
+        legends.push({
+            caption: "pending",
+            color: c.VERDICT_UNKNOWN2,
+        });
+        legends.push({
+            caption: "incorrect",
+            color: c.VERDICT_NOK2,
+        });
         bars.push(...statistics?.map(({result, success, pending, wrong}, index) => ({
             name: tasks[index].letter,
             color: tasks[index].color,
@@ -67,6 +86,14 @@ const stackedBarsData = (resultType, tasks, statistics, count) => {
             ]
         })));
     } else if (resultType === "IOI") {
+        legends.push({
+            caption: "max score",
+            color: c.VERDICT_OK2,
+        });
+        legends.push({
+            caption: "min score",
+            color: c.VERDICT_NOK2,
+        });
         bars.push(...statistics?.map(({ result, success, pending, wrong }, index) => ({
             name: tasks[index].letter,
             color: tasks[index].color,
@@ -77,7 +104,7 @@ const stackedBarsData = (resultType, tasks, statistics, count) => {
         })));
     }
     return {
-        legend: [],
+        legends: legends,
         bars: bars,
     };
 }
@@ -92,14 +119,19 @@ export const Statistics = ({ widgetData: { location } }) => {
         Math.floor((location.sizeY - 60) / (c.STATISTICS_BAR_HEIGHT_PX + c.STATISTICS_BAR_GAP_PX))
     );
 
+    const data = stackedBarsData(resultType, tasks, statistics, count);
+
     return (
         <StatisticsWrap>
             <StatisticsHeader>
-                <StatisticsHeaderTitle>{c.STATISTICS_TITLE}</StatisticsHeaderTitle>
-                <StatisticsHeaderCaption>{c.STATISTICS_CAPTION}</StatisticsHeaderCaption>
+                <StatisticsHeaderWrapper>
+                    <StatisticsHeaderTitle>{c.STATISTICS_TITLE}</StatisticsHeaderTitle>
+                    <StatisticsHeaderCaption>{c.STATISTICS_CAPTION}</StatisticsHeaderCaption>
+                </StatisticsHeaderWrapper>
+                <StatisticsLegends legends={data.legends}></StatisticsLegends>
             </StatisticsHeader>
 
-            <StackedBarsStatistics data={stackedBarsData(resultType, tasks, statistics, count)} rowsCount={rowsCount}/>
+            <StackedBarsStatistics data={data} rowsCount={rowsCount}/>
         </StatisticsWrap>
     )
 };
