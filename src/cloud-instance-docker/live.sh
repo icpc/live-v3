fee44045226ba809fe9a0e6c95623b75922bdf71
Azat Ismagilov <azatismagilov00@gmail.com>
2023-11-27 22:06:07 +0000

    Update supervisor config

--- src/cloud-instance-docker/live.sh
+++ src/cloud-instance-docker/live.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec java -jar live.jar --port=8080 --config-directory=config --creds=creds.json > output.log 2> error.log
+exec java -jar live.jar --port=8080 --config-directory=config --creds=creds.json
