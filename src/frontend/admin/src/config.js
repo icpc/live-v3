c9dba52719279f62b0ef4a6e788843e2b22aea06
Kostya Bats <kbats@bk.ru>
2024-02-09 19:31:53 +0300

    Admin: header and favicon color from visual config

--- src/frontend/admin/src/config.js
+++ src/frontend/admin/src/config.js
@@ -1,8 +1,9 @@
 const BACKEND_PROTO = window.location.protocol === "https:" ? "https://" : "http://";
 const BACKEND_PORT = process.env.REACT_APP_BACKEND_PORT ?? window.location.port;
-export const BASE_URL_BACKEND = process.env.REACT_APP_BACKEND_URL ?? (BACKEND_PROTO + window.location.hostname  + ":" + BACKEND_PORT + "/api/admin");
-export const OVERLAY_LOCATION = process.env.REACT_APP_OVERLAY_LOCATION ?? (BACKEND_PROTO + window.location.hostname  + ":" + BACKEND_PORT + "/overlay");
-export const SCHEMAS_LOCATION = process.env.REACT_APP_SCHEMAS_LOCATION ?? (BACKEND_PROTO + window.location.hostname  + ":" + BACKEND_PORT + "/schemas");
+export const BACKEND_ROOT = process.env.REACT_APP_BACKEND_ROOT ?? (BACKEND_PROTO + window.location.hostname  + ":" + BACKEND_PORT);
+export const BASE_URL_BACKEND = process.env.REACT_APP_BACKEND_URL ?? (BACKEND_ROOT + "/api/admin");
+export const OVERLAY_LOCATION = process.env.REACT_APP_OVERLAY_LOCATION ?? (BACKEND_ROOT + "/overlay");
+export const SCHEMAS_LOCATION = process.env.REACT_APP_SCHEMAS_LOCATION ?? (BACKEND_ROOT + "/schemas");
 
 const WS_PROTO = window.location.protocol === "https:" ? "wss://" : "ws://";
 export const BASE_URL_WS = process.env.REACT_APP_WEBSOCKET_URL ?? (WS_PROTO + window.location.hostname + ":" + BACKEND_PORT + "/api/admin");
