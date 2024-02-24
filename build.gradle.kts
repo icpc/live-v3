a87d6f8ef99245bd09ba3937f689a703334d68d2
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2024-02-24 17:28:45 +0100

    Fix a lib publication version

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,6 +64,7 @@ subprojects {
                         }
                         from(components["java"])
                         groupId = "org.icpclive"
+                        version = rootProject.findProperty("build_version")!!.toString()
                         artifactId = when (val name = this@subprojects.name){
                             "plugins" -> "org.icpclive.cds.full"
                             else -> "org.icpclive.cds.$name"
