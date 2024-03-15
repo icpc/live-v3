84c29ed6fc6822d4b6a9d7cdecdeb266421e0e5d
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2024-03-15 23:34:35 +0100

    Add two days test

--- src/cds/plugins/ejudge/src/test/kotlin/EjudgeTest.kt
+++ src/cds/plugins/ejudge/src/test/kotlin/EjudgeTest.kt
@@ -1,6 +1,7 @@
 package org.icpclive.cds.plugins.ejudge
 
 import org.icpclive.cds.CdsLoadersTest
+import org.icpclive.cds.api.ContestResultType
 import org.icpclive.cds.settings.*
 import kotlin.test.Test
 
@@ -12,6 +13,30 @@ object EjudgeTest : CdsLoadersTest() {
             EjudgeSettings(
                 url = UrlOrLocalPath.Local(testDataDir.resolve("ejudge.xml"))
             )
+        )
+    }
+
+    @Test
+    fun ejudgeTwoDays() {
+        loaderTest(
+            goldenDataDir.resolve("ejudgeTwoDays.txt"),
+            EjudgeSettings(
+                url = UrlOrLocalPath.Local(testDataDir.resolve("ejudge-ioi-day2.xml")),
+            ) {
+                resultType = ContestResultType.IOI
+                previousDays = listOf(
+                    PreviousDaySettings(
+                        EjudgeSettings(
+                            url = UrlOrLocalPath.Local(testDataDir.resolve("ejudge-ioi-day1.xml"))
+                        ) {
+                            resultType = ContestResultType.IOI
+                            problemScoreLimit = mapOf(
+                                "5" to 71.0,
+                                "8" to 84.0,
+                            )
+                        })
+                )
+            }
         )
     }
 
