8e271062e559c9f9b60821e7cc1ec0d91b47aff3
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2024-04-16 21:36:53 +0200

    Update test data

--- src/cds/tests/src/main/kotlin/org/icpclive/cds/CdsLoadersTest.kt
+++ src/cds/tests/src/main/kotlin/org/icpclive/cds/CdsLoadersTest.kt
@@ -23,7 +23,7 @@ abstract class CdsLoadersTest {
         .relativeTo(Path.of("").absolute())
 
     protected val goldenDataDir: Path = testDataDir.resolve("goldenData")
-    private val updateTestData = true
+    private val updateTestData = false
 
 
     private val json = Json {
