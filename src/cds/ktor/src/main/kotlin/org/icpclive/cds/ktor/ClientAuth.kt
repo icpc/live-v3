ce30250d3a7f4413b15f516acf9a23348c4146d6
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2024-06-02 10:48:15 +0200

    Support passing auth at url.

--- src/cds/ktor/src/main/kotlin/org/icpclive/cds/ktor/ClientAuth.kt
+++ src/cds/ktor/src/main/kotlin/org/icpclive/cds/ktor/ClientAuth.kt
@@ -2,10 +2,11 @@ package org.icpclive.cds.ktor
 
 import io.ktor.client.*
 import io.ktor.client.plugins.*
-import io.ktor.client.plugins.auth.*
 import io.ktor.client.plugins.auth.providers.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import org.icpclive.cds.settings.*
+import io.ktor.client.plugins.auth.Auth as AuthPlugin
 
 internal class HeaderAuthImpl(private val header: String, private val value: String) : ClientAuth {
     override fun setupAuth(config: HttpClientConfig<*>) {
@@ -17,7 +18,7 @@ internal class HeaderAuthImpl(private val header: String, private val value: Str
 
 internal class BasicAuthImpl(private val login: String, private val password: String) : ClientAuth {
     override fun setupAuth(config: HttpClientConfig<*>) {
-        config.install(Auth) {
+        config.install(AuthPlugin) {
             basic {
                 credentials { BasicAuthCredentials(this@BasicAuthImpl.login, this@BasicAuthImpl.password) }
                 sendWithoutRequest { true }
@@ -50,3 +51,21 @@ public interface ClientAuth {
             }
     }
 }
+
+public fun HttpRequestBuilder.setupAuth(auth: Auth?) {
+    if (auth == null) return
+    auth.basic?.let { basicAuth(it.login.value, it.password.value) }
+    for ((name, value) in auth.cookies) {
+        cookie(name, value.value)
+    }
+    for ((name, value) in auth.headers) {
+        header(name, value)
+    }
+}
+
+public fun Auth.withOAuth(token: Credential): Auth = withHeader(HttpHeaders.Authorization, Credential("OAuth ${token.value}", "OAuth ${token.displayValue}"))
+public fun Auth.withBearer(token: Credential): Auth = withHeader(HttpHeaders.Authorization, Credential("Bearer ${token.value}", "Bearer ${token.displayValue}"))
+
+public fun UrlOrLocalPath.Url.withOAuth(token: Credential): UrlOrLocalPath.Url = withHeader(HttpHeaders.Authorization, Credential("OAuth ${token.value}", "OAuth ${token.displayValue}"))
+public fun UrlOrLocalPath.Url.withBearer(token: Credential): UrlOrLocalPath.Url = withHeader(HttpHeaders.Authorization, Credential("Bearer ${token.value}", "Bearer ${token.displayValue}"))
+
