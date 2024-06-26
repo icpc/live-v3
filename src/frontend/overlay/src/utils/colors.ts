9cba7ef9e412c145842d313abdcfd33c842ccd55
Kornilovich Mikhail <kornilovichmihail@gmail.com>
2024-06-26 14:32:09 +0300

    fix text color for advertisement

--- src/frontend/overlay/src/utils/colors.ts
+++ src/frontend/overlay/src/utils/colors.ts
@@ -1,9 +1,18 @@
 export const hexToRgb = (hex: string): {r: number, g: number, b: number} | null => {
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2}?)$/i.exec(hex);
+    if (hex.length !== 4 && hex.length !== 7 && hex.length !== 9) {
+        return null;
+    }
+
+    const result = /^#?([a-f\d]{1,2})([a-f\d]{1,2})([a-f\d]{1,2})([a-f\d]{1,2})?$/i.exec(hex);
+
+    const getLastPartOfHex = (code: string): string => {
+        return code.length == 1 ? code : "";
+    };
+
     return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
+        r: parseInt(result[1] + getLastPartOfHex(result[1]), 16),
+        g: parseInt(result[2] + getLastPartOfHex(result[2]), 16),
+        b: parseInt(result[3] + getLastPartOfHex(result[3]), 16)
     } : null;
 };
 
