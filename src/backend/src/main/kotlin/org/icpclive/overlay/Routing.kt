4e8ca23893db4238fc159ddc81c704a946a115fd
Kornilovich Mikhail <kornilovichmihail@gmail.com>
2024-07-20 09:58:24 +0300

    ignore run after accepted verdict

--- src/backend/src/main/kotlin/org/icpclive/overlay/Routing.kt
+++ src/backend/src/main/kotlin/org/icpclive/overlay/Routing.kt
@@ -14,7 +14,7 @@ import org.icpclive.data.DataBus
 import org.icpclive.data.currentContestInfoFlow
 import org.icpclive.util.sendJsonFlow
 
-inline fun <reified T: Any> Route.flowEndpoint(name: String, crossinline dataProvider: suspend () -> Flow<T>) {
+inline fun <reified T : Any> Route.flowEndpoint(name: String, crossinline dataProvider: suspend () -> Flow<T>) {
     webSocket(name) { sendJsonFlow(dataProvider()) }
     get(name) { call.respond(dataProvider().first()) }
 }
@@ -36,23 +36,36 @@ fun Route.configureOverlayRouting() {
             return@webSocket
         }
         val teamId = teamIdStr.toTeamId()
-        sendJsonFlow(DataBus.contestStateFlow.await().map { state -> state.runsAfterEvent.values.filter { it.teamId == teamId }.sortedBy { it.time } }.distinctUntilChanged()
+        sendJsonFlow(DataBus.contestStateFlow.await().map { state ->
+            state.runsAfterEvent.values.filter { it.teamId == teamId }
+                .sortedBy { it.time }
+        }.distinctUntilChanged()
             .map { runs ->
+                val acceptedProblems = mutableSetOf<ProblemId>()
                 runs.map { info ->
                     when (info.result) {
                         is RunResult.ICPC -> {
                             val icpcResult = info.result as RunResult.ICPC
-                            TimeLineRunInfo.ICPC(info.time, info.problemId, icpcResult.verdict.isAccepted)
+                            if (!acceptedProblems.contains(info.problemId)) {
+                                if (icpcResult.verdict == Verdict.Accepted) {
+                                    acceptedProblems.add(info.problemId)
+                                }
+                                TimeLineRunInfo.ICPC(info.time, info.problemId, icpcResult.verdict.isAccepted)
+                            } else {
+                                null
+                            }
                         }
+
                         is RunResult.IOI -> {
                             val ioiResult = info.result as RunResult.IOI
                             TimeLineRunInfo.IOI(info.time, info.problemId, ioiResult.scoreAfter)
                         }
+
                         else -> {
                             TimeLineRunInfo.InProgress(info.time, info.problemId)
                         }
                     }
-                }
+                }.filterNotNull()
             })
     }
     flowEndpoint("/queue") { DataBus.queueFlow.await() }
@@ -61,7 +74,7 @@ fun Route.configureOverlayRouting() {
     route("/scoreboard") {
         setUpScoreboard { getScoreboardDiffs(it) }
     }
-    route("/svgAchievement"){
+    route("/svgAchievement") {
         configureSvgAchievementRouting(Config.mediaDirectory)
     }
     get("/visualConfig.json") { call.respond(DataBus.visualConfigFlow.await().value) }
