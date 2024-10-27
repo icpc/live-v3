177f8f996aa2d359cdf2767381063bd492a53792
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2024-10-27 10:00:35 +0100

    Remove commented debug

--- src/cds/core/src/main/kotlin/org/icpclive/cds/adapters/impl/RemoveFrozenSubmissionsAdapter.kt
+++ src/cds/core/src/main/kotlin/org/icpclive/cds/adapters/impl/RemoveFrozenSubmissionsAdapter.kt
@@ -12,7 +12,6 @@ internal fun Flow<ContestUpdate>.processByTimeCut(
     process: (RunInfo) -> RunInfo
 ): Flow<ContestUpdate> = contestState().transform {
     suspend fun emit(info: RunInfo, cut: Duration) {
-//        println("${info.id} ${info.time} ${info.result} $cut ${it.lastEvent}")
         if (info.time > cut) {
             emit(RunUpdate(process(info)))
         } else {
@@ -31,13 +30,12 @@ internal fun Flow<ContestUpdate>.processByTimeCut(
             if (oldCut != newCut) {
                 it.runsAfterEvent.values
                     .filter { run -> (run.time > oldCut) != (run.time > newCut) }
-//                    .also { println("Updated runs: ${it.map { run -> run.id }}")}
                     .forEach { run -> emit(run, newCut) }
             }
         }
         is CommentaryMessagesUpdate -> emit(it.lastEvent)
     }
-}.onEach { /* if (it is RunUpdate) println("${it.newInfo.id} ${it.newInfo.time} ${it.newInfo.result}") */ }
+}
 
 internal fun removeFrozenSubmissionsResults(flow: Flow<ContestUpdate>): Flow<ContestUpdate> =
     flow.processByTimeCut(
