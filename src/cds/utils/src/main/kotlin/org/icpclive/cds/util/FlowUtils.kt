966467770e06727a831fcc6b34df5ceb304381c3
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2025-04-07 20:19:20 +0200

    Replace onIdle with suggestions by kotlinx.coroutines experts

--- src/cds/utils/src/main/kotlin/org/icpclive/cds/util/FlowUtils.kt
+++ src/cds/utils/src/main/kotlin/org/icpclive/cds/util/FlowUtils.kt
@@ -3,10 +3,9 @@ package org.icpclive.cds.util
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.ProducerScope
 import kotlinx.coroutines.flow.*
-import java.util.concurrent.atomic.AtomicReference
+import kotlinx.coroutines.selects.onTimeout
+import kotlinx.coroutines.selects.select
 import kotlin.time.Duration
-import kotlin.time.TimeMark
-import kotlin.time.TimeSource
 
 public fun <T> Flow<T>.logAndRetryWithDelay(duration: Duration, log: (Throwable) -> Unit): Flow<T> = retryWhen { cause: Throwable, _: Long ->
     if (cause is CancellationException) {
@@ -31,20 +30,22 @@ public fun <T> runCatchingIfNotCancellation(block: () -> T): Result<T> = runCatc
     if (exception is CancellationException) throw exception
 }
 
+@OptIn(ExperimentalCoroutinesApi::class)
 public fun <T> Flow<T>.onIdle(interval: Duration, block: suspend ProducerScope<T>.() -> Unit) = channelFlow {
-    val r = AtomicReference<TimeMark>(TimeSource.Monotonic.markNow())
-    val job = launch {
-        while (true) {
-            delay(interval - r.get().elapsedNow())
-            if (r.get().elapsedNow() > interval) {
+    val data = produceIn(this)
+    var finished = false
+    while (!finished) {
+        select {
+            data.onReceiveCatching {
+                if (it.isSuccess) {
+                    send(it.getOrThrow())
+                } else {
+                    finished = true
+                }
+            }
+            onTimeout(interval) {
                 block()
-                r.set(TimeSource.Monotonic.markNow())
             }
         }
     }
-    collect {
-        channel.send(it)
-        r.set(TimeSource.Monotonic.markNow())
-    }
-    job.cancel()
 }
