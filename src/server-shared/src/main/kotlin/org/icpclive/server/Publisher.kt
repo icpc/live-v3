1bf89ca719be971c00ec162383ee8f45a036b5fb
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2025-06-14 20:44:29 +0200

    Support for publish-command

--- src/server-shared/src/main/kotlin/org/icpclive/server/Publisher.kt
+++ src/server-shared/src/main/kotlin/org/icpclive/server/Publisher.kt
@@ -1,29 +1,17 @@
 package org.icpclive.server
 
 import com.github.ajalt.clikt.parameters.groups.OptionGroup
-import com.github.ajalt.clikt.parameters.options.convert
-import com.github.ajalt.clikt.parameters.options.multiple
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.required
-import com.github.ajalt.clikt.parameters.options.validate
+import com.github.ajalt.clikt.parameters.options.*
 import com.github.ajalt.clikt.parameters.types.int
-import io.ktor.client.HttpClient
-import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.request.get
-import io.ktor.client.statement.bodyAsText
+import io.ktor.client.*
+import io.ktor.client.plugins.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.server.application.Application
-import io.ktor.server.application.ApplicationCall
 import io.ktor.server.application.port
-import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import org.icpclive.cds.util.getLogger
-import kotlin.io.path.Path
-import kotlin.io.path.absolute
-import kotlin.io.path.exists
-import kotlin.io.path.moveTo
-import kotlin.io.path.name
-import kotlin.io.path.writeText
+import kotlin.io.path.*
 import kotlin.time.Duration.Companion.seconds
 
 class Publisher : OptionGroup("publisher") {
@@ -39,12 +27,15 @@ class Publisher : OptionGroup("publisher") {
             require(p.parent.exists()) { "Directory ${p.parent} does not exist" }
         }
     }
+    val command by option("--publish-command", help = "Execute a callback command after publish is done.")
 }
 
 private val log by getLogger()
 
 fun Application.startPublisher(config: Publisher) {
-    log.info { "Stating publiser with interval ${config.interval} seconds and paths ${config.paths}" }
+    log.info { "Stating publisher with interval ${config.interval} seconds and paths ${config.paths}" }
+
+
     launch(CoroutineName("publisher")) {
         while (true) {
             delay(config.interval)
@@ -59,6 +50,26 @@ fun Application.startPublisher(config: Publisher) {
                 val tmpP = p.resolveSibling(p.name + ".tmp")
                 tmpP.writeText(data)
                 tmpP.moveTo(p, overwrite = true)
+            }
+            config.command?.let { command ->
+                withContext(Dispatchers.IO) {
+                    try {
+                        val commandList = command.split(" ")
+                        log.info { "Executing command: ${commandList.joinToString(" ")}" }
+                        val processBuilder = ProcessBuilder(commandList)
+                        val process = processBuilder.start()
+                        val exitCode = process.waitFor()
+                        val stdOutput = process.inputStream.bufferedReader().readText()
+                        if (exitCode != 0) {
+                            val errorOutput = process.errorStream.bufferedReader().readText()
+                            log.error { "Command execution failed with exit code $exitCode. Error output: $errorOutput" }
+                        } else {
+                            log.info { "Command executed successfully. Output: $stdOutput" }
+                        }
+                    } catch (e: Exception) {
+                        log.error(e) { "Failed to execute command" }
+                    }
+                }
             }
         }
     }
