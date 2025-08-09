e32a76b31bbdd06a6c1e0a601d29d815a32b2fa5
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2025-08-09 12:04:14 +0200

    Rewrite templates in tuning to be reusable

--- src/cds/core/src/main/kotlin/org/icpclive/cds/tunning/Templates.kt
+++ src/cds/core/src/main/kotlin/org/icpclive/cds/tunning/Templates.kt
@@ -2,127 +2,91 @@ package org.icpclive.cds.tunning
 
 import io.ktor.http.*
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonNull
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.encodeToJsonElement
 import org.icpclive.cds.api.*
 import org.icpclive.cds.util.logger
 import org.icpclive.cds.util.runCatchingIfNotCancellation
 
+
 private val templateRegex = Regex("\\{(!?[a-z0-9A-Z_.-]*)}")
 
-private fun String.getTemplateValue(
-    team: TeamInfo?,
-    org: OrganizationInfo?,
-    extraValues: Map<String, String>,
-    isUrl: Boolean
-): String {
-    val provider = fun(it: String): String? {
-        return when {
-            it.startsWith("team.") -> {
-                if (team == null) {
-                    null
-                } else {
-                    when (it.removePrefix("team.")) {
-                        "id" -> team.id.value
-                        "displayName" -> team.displayName
-                        "fullName" -> team.fullName
-                        "hashTag" -> team.hashTag
-                        "color" -> team.color.toString()
-                        else -> null
-                    }
-                }
-            }
-
-            it.startsWith("org.") -> {
-                if (org == null) {
-                    null
-                } else {
-                    when (it.removePrefix("org.")) {
-                        "id" -> org.id.value
-                        "displayName" -> org.displayName
-                        "fullName" -> org.fullName
-                        else -> null
-                    }
-                }
-            }
-
-            it.startsWith("regexes.") -> extraValues[it.removePrefix("regexes.")]
-            else -> team?.customFields[it]
-        }
-    }
-    return replace(templateRegex) {
-        val str = it.groups[1]!!.value
-        if (isUrl) {
-            if (str.startsWith("!")) {
-                provider(str.removePrefix("!"))
-            } else {
-                provider(str)?.encodeURLParameter()
-            }
-        } else {
-            provider(str)
-        } ?: it.value
-    }
+public interface TemplateSubstitutor {
+    public fun substitute(data: String, sanitizer: (String) -> String): String
 }
 
+public fun TemplateSubstitutor.substituteUrlSafe(data: String): String = substitute(data) { it.encodeURLParameter() }
 
-internal interface TemplateSubstitutor {
-    fun String.substituteUrl(): String
-    fun String.substituteRegular(): String
-
-    fun MediaType.substitute(): MediaType {
-        return when (this) {
-            is MediaType.Image -> copy(url = url.substituteUrl())
-            is MediaType.Video -> copy(url = url.substituteUrl())
-            is MediaType.M2tsVideo -> copy(url = url.substituteUrl())
-            is MediaType.HLSVideo -> copy(url = url.substituteUrl())
-            is MediaType.Object -> copy(url = url.substituteUrl())
-            is MediaType.WebRTCProxyConnection -> copy(url = this.url.substituteUrl())
-            is MediaType.WebRTCGrabberConnection -> copy(
-                url = url.substituteUrl(),
-                peerName = peerName.substituteRegular(),
-                credential = credential?.substituteRegular()
-            )
-
-            else -> this
-        }
-
+public fun TemplateSubstitutor.substitute(type: MediaType): MediaType {
+    return when (type) {
+        is MediaType.Image -> type.copy(url = substituteUrlSafe(type.url))
+        is MediaType.Video -> type.copy(url = substituteUrlSafe(type.url))
+        is MediaType.M2tsVideo -> type.copy(url = substituteUrlSafe(type.url))
+        is MediaType.HLSVideo -> type.copy(url = substituteUrlSafe(type.url))
+        is MediaType.Object -> type.copy(url = substituteUrlSafe(type.url))
+        is MediaType.WebRTCProxyConnection -> type.copy(url = substituteUrlSafe(type.url))
+        is MediaType.WebRTCGrabberConnection -> type.copy(
+            url = substituteUrlSafe(type.url),
+            peerName = substituteRaw(type.peerName),
+            credential = type.credential?.let { substituteRaw(it) }
+        )
+        else -> type
     }
+}
+public fun TemplateSubstitutor.substituteRaw(data: String): String = substitute(data) { it }
+
+@JvmName("substituteRawNullable")
+public fun TemplateSubstitutor.substituteRaw(data: String?): String? = data?.let { substituteRaw(it) }
+@JvmName("substituteUrlNullable")
+public fun TemplateSubstitutor.substituteUrlSafe(data: String?): String? = data?.let { substituteUrlSafe(it) }
+@JvmName("substituteNullable")
+public fun TemplateSubstitutor.substitute(data: MediaType?): MediaType? = data?.let { substitute(it) }
+
+
+internal class TemplateSubstitutorImpl(private val data: JsonObject) : TemplateSubstitutor {
+    private fun getByKey(key: String): String? {
+        var cur = data
+        val parts = key.split(".")
+        for (i in parts.dropLast(1)) {
+            cur = cur[i] as? JsonObject ?: return null
+        }
+        val result = cur[parts.last()] ?: return null
+        return when (result) {
+            is JsonArray, is JsonObject -> Json.encodeToString(result)
+            is JsonPrimitive -> result.content
+            JsonNull -> null
+        }
+    }
+
+
+    private fun String.getTemplateValue(
+        sanitizer: (String) -> String
+    ): String {
+        return replace(templateRegex) {
+            val str = it.groups[1]!!.value
+            if (str.startsWith("!")) {
+                getByKey(str.removePrefix("!"))
+            } else {
+                getByKey(str)?.let(sanitizer)
+            } ?: it.value
+        }
+    }
+
+
+    override fun substitute(data: String, sanitizer: (String) -> String): String = data.getTemplateValue(sanitizer)
 }
 
 internal fun getSubstitutor(regexes: Map<String, TemplateRegexParser>, teamInfo: TeamInfo?, orgInfo: OrganizationInfo?): TemplateSubstitutor {
-    val customValues = buildMap {
-        fun <T> Result<T>.getOrNullAndWarn(regex: Regex, value: String, replacement: String? = null): T? {
-            exceptionOrNull()?.let {
-                logger(OverrideTeamTemplate::class).error(it) {
-                    "Problems during executing regular expression `${regex}` on value `$value` with replacement `$replacement`"
-                }
-            }
-            return getOrNull()
-        }
-        for ((blockName, parser) in regexes) {
-            val fromValue = parser.from.getTemplateValue(teamInfo, orgInfo, this, false)
-            for ((regex, extras) in parser.rules) {
-                val match = runCatchingIfNotCancellation {
-                    regex.matchEntire(fromValue)
-                }.getOrNullAndWarn(regex, fromValue, null) ?: continue
-                for ((index, group) in match.groupValues.withIndex().drop(1)) {
-                    put("$blockName.$index", group)
-                }
-                for ((key, value) in extras) {
-                    put("$blockName.$key", runCatchingIfNotCancellation {
-                        regex.replace(fromValue, value)
-                    }.getOrNullAndWarn(regex, fromValue, value) ?: continue)
-                }
-                break
-            }
-        }
-    }
-    return object : TemplateSubstitutor {
-        override fun String.substituteRegular(): String {
-            return this.getTemplateValue(teamInfo, orgInfo, customValues, false)
-        }
-
-        override fun String.substituteUrl(): String {
-            return this.getTemplateValue(teamInfo, orgInfo, customValues, true)
-        }
+    return buildTemplateSubstitutor {
+        teamInfo?.let(::addTeam)
+        orgInfo?.let(::addOrganization)
+        regexes.forEach { (name, parser) -> addRegex(name, parser) }
     }
 }
 
@@ -136,3 +100,65 @@ public class TemplateRegexParser(
 )
 
 internal fun String.hasNoUnsubstitutedRegex() = takeUnless { it.contains(templateRegex) }
+
+public class TemplateSubstitutionBuilder @PublishedApi internal constructor() {
+    private var map = mutableMapOf<String, JsonElement>()
+
+    public fun addJson(key: String, value: JsonElement) {
+        map[key] = value
+    }
+    public fun addTeam(info: TeamInfo, name: String = "team", withFlatCustomFields: Boolean = true) {
+        val teamJson = Json.encodeToJsonElement(info) as JsonObject
+        addJson(name, buildJsonObject {
+            for ((k, v) in teamJson) {
+                put(k, v)
+                if (k == "name") put("fullName", v)
+                if (k == "shortName") put("displayName", v)
+            }
+        })
+        if (withFlatCustomFields) {
+            for ((k, v) in (teamJson["customFields"] as? JsonObject).orEmpty()) {
+                addJson(k, v)
+            }
+        }
+    }
+    public fun addOrganization(info: OrganizationInfo, name: String = "org") {
+        addJson(name, Json.encodeToJsonElement(info) as JsonObject)
+    }
+    public fun addRegex(groupName: String, parser: TemplateRegexParser, name: String = "regexes") : TemplateSubstitutionBuilder = apply {
+        fun <T> Result<T>.getOrNullAndWarn(regex: Regex, value: String, replacement: String? = null): T? {
+            exceptionOrNull()?.let {
+                logger(OverrideTeamTemplate::class).error(it) {
+                    "Problems during executing regular expression `${regex}` on value `$value` with replacement `$replacement`"
+                }
+            }
+            return getOrNull()
+        }
+
+        val fromValue = with (build()) { substituteRaw(parser.from) }
+        val result = buildMap {
+            for ((regex, extras) in parser.rules) {
+                val match = runCatchingIfNotCancellation {
+                    regex.matchEntire(fromValue)
+                }.getOrNullAndWarn(regex, fromValue, null) ?: continue
+                for ((index, group) in match.groupValues.withIndex().drop(1)) {
+                    put("$index", group)
+                }
+                for ((key, value) in extras) {
+                    put(key, runCatchingIfNotCancellation {
+                        regex.replace(fromValue, value)
+                    }.getOrNullAndWarn(regex, fromValue, value) ?: continue)
+                }
+                break
+            }
+        }
+        val oldValue = (map[name] as? JsonObject)
+        val newValue = mapOf(groupName to JsonObject(result.mapValues { JsonPrimitive(it.value) })) + oldValue.orEmpty()
+        addJson(name, JsonObject(newValue))
+    }
+
+    @PublishedApi
+    internal fun build(): TemplateSubstitutor = TemplateSubstitutorImpl(JsonObject(map))
+}
+
+public inline fun buildTemplateSubstitutor(block: TemplateSubstitutionBuilder.() -> Unit): TemplateSubstitutor = TemplateSubstitutionBuilder().apply(block).build()
