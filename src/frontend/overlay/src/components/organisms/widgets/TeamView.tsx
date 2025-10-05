cd2d43be70203909f60ee061b339acda7a506459
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2025-10-05 17:32:02 +0200

    Fix type-check errors

--- src/frontend/overlay/src/components/organisms/widgets/TeamView.tsx
+++ src/frontend/overlay/src/components/organisms/widgets/TeamView.tsx
@@ -344,7 +344,7 @@ type TeamViewSingleContentProps = OverlayWidgetProps<Widget.TeamViewWidget> & {
 
 export const TeamViewSingleContent = ({ widgetData: { settings, widgetLocationId }, transitionState, onLoaded }: TeamViewSingleContentProps) => {
     const { primary, secondary, achievement, position } = settings;
-    const location = c.WIDGET_POSITIONS[widgetLocationId];
+    const location = c.WIDGET_POSITIONS[widgetLocationId] as LocationRectangle;
     const variant = teamViewVariant(position);
 
     const [primaryLoaded, setPrimaryLoaded] = useState(false);
