84834919013fafaec0369960b4514a09138ba322
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2025-11-12 18:22:51 +0100

    Fix gen ts caching in gradle.

--- build-logic/src/main/kotlin/org/icpclive/gradle/tasks/TsInterfaceGeneratorTask.kt
+++ build-logic/src/main/kotlin/org/icpclive/gradle/tasks/TsInterfaceGeneratorTask.kt
@@ -1,9 +1,9 @@
 package org.icpclive.gradle.tasks
 
 import dev.adamko.kxstsgen.KxsTsGenerator
-import gradle.kotlin.dsl.accessors._0efe46a4cec2e9e682da24afc1fcb716.main
+import dev.adamko.kxstsgen.core.SerializerDescriptorsExtractor
+import dev.adamko.kxstsgen.core.util.MutableMapWithDefaultPut
 import gradle.kotlin.dsl.accessors._0efe46a4cec2e9e682da24afc1fcb716.runtimeClasspath
-import gradle.kotlin.dsl.accessors._0efe46a4cec2e9e682da24afc1fcb716.sourceSets
 import kotlinx.serialization.serializer
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
@@ -12,6 +12,8 @@ import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.*
 import java.net.URLClassLoader
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
 
 
 abstract class TsInterfaceGeneratorTask : DefaultTask() {
@@ -38,8 +40,23 @@ abstract class TsInterfaceGeneratorTask : DefaultTask() {
         outputLocation.convention(fileName.flatMap { project.layout.buildDirectory.file("ts/${it}.ts") })
     }
 
+    // TODO: this is dirty hack
+    // correct way is to run the task via Worker API, with loading this library only for a task.
+    private fun clearKxsTsGeneratorCaches() {
+        val cacheProperty = SerializerDescriptorsExtractor.Default::class.memberProperties.single { it.name == "elementDescriptors" }
+        cacheProperty.isAccessible = true
+        val delegate = cacheProperty.getDelegate(SerializerDescriptorsExtractor.Default) as MutableMapWithDefaultPut<*, *>
+        cacheProperty.isAccessible = false
+        val backingMapProperty = delegate::class.memberProperties.single { it.name == "map" }
+        backingMapProperty.isAccessible = true
+        val backingMap = backingMapProperty.call(delegate) as MutableMap<*, *>
+        backingMapProperty.isAccessible = false
+        backingMap.clear()
+    }
+
     @TaskAction
     fun generate() {
+        clearKxsTsGeneratorCaches()
         val taskClassLoader = Thread.currentThread().getContextClassLoader();
         val targetClassUrls = classpath.files.map { it.toURI().toURL() }.toTypedArray();
         URLClassLoader(targetClassUrls, taskClassLoader).use { classLoader ->
