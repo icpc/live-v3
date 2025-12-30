1e6d3f84cf27c3f1e9d2373479c4e78f9fb5f3c0
Mikhail Kornilovich <kornilovichmihail@gmail.com>
2025-12-30 17:01:55 +0300

    optimizing animations

--- src/frontend/overlay/src/components/atoms/ContestLabels.jsx
+++ src/frontend/overlay/src/components/atoms/ContestLabels.jsx
@@ -162,6 +162,12 @@ export const RunStatusLabel = ({ runInfo, className }) => {
 };
 
 const TaskResultLabelWrapper2 = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
     font-weight: bold;
     color: #fff;
 
