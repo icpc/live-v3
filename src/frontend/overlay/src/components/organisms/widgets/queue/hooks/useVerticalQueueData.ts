0e866c7597f5deb055d30747f04899e04a5696c8
Arslan 'ArsNotFound' Sakhapov <arslan.sakhapov@yandex.ru>
2026-02-13 16:46:17 +0500

    Fix queue fts rows cutting

--- src/frontend/overlay/src/components/organisms/widgets/queue/hooks/useVerticalQueueData.ts
+++ src/frontend/overlay/src/components/organisms/widgets/queue/hooks/useVerticalQueueData.ts
@@ -18,7 +18,7 @@ export function useVerticalQueueRowsData({
     }
 
     const allowedMaxRows = Math.min(
-        (_width / c.QUEUE_ROW_WIDTH) * (height / c.QUEUE_ROW_HEIGHT),
+        Math.floor((_width / c.QUEUE_ROW_WIDTH) * ((height - c.QUEUE_FTS_BOTTOM_OFFSET) / (c.QUEUE_ROW_HEIGHT + c.QUEUE_ROW_Y_PADDING))),
         c.QUEUE_MAX_ROWS,
     );
 
@@ -65,8 +65,7 @@ export function useVerticalQueueRowsData({
         if (row.isFts) {
             row.bottom =
                 height -
-                bottomPosition(totalFts - ftsRowCount) +
-                c.QUEUE_FTS_BOTTOM_OFFSET;
+                bottomPosition(totalFts - ftsRowCount);
             ftsRowCount++;
         } else {
             row.bottom = bottomPosition(regularRowCount);
