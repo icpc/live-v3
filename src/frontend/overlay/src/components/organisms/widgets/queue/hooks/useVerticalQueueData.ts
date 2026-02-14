e00d77edceed0f1bda4132ed22220516fa80ec5a
Arslan 'ArsNotFound' Sakhapov <arslan.sakhapov@yandex.ru>
2026-02-14 19:29:45 +0500

    Revert "Rename QUEUE_FTS_BOTTOM_OFFSET to QUEUE_FTS_BOTTOM_MIN_OFFSET"
    This reverts commit a06994cb

--- src/frontend/overlay/src/components/organisms/widgets/queue/hooks/useVerticalQueueData.ts
+++ src/frontend/overlay/src/components/organisms/widgets/queue/hooks/useVerticalQueueData.ts
@@ -20,7 +20,7 @@ export function useVerticalQueueRowsData({
     const allowedMaxRows = Math.min(
         Math.floor(
             (_width / c.QUEUE_ROW_WIDTH) *
-                ((height - c.QUEUE_FTS_BOTTOM_MIN_OFFSET) /
+                ((height - c.QUEUE_FTS_BOTTOM_OFFSET) /
                     (c.QUEUE_ROW_HEIGHT + c.QUEUE_ROW_Y_PADDING)),
         ),
         c.QUEUE_MAX_ROWS,
