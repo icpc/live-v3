ba48d1d5af426a69a5e6ef948cc4d813381b1f42
Arslan 'ArsNotFound' Sakhapov <arslan.sakhapov@yandex.ru>
2026-03-08 22:27:24 +0500

    Move hidden teams filter to TeamSpotlightService flow generator

--- src/backend/src/main/kotlin/org/icpclive/controllers/TeamViewController.kt
+++ src/backend/src/main/kotlin/org/icpclive/controllers/TeamViewController.kt
@@ -1,7 +1,6 @@
 package org.icpclive.controllers
 
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.filter
 import org.icpclive.api.*
 import org.icpclive.cds.api.TeamMediaType
 import org.icpclive.data.*
@@ -35,16 +34,11 @@ class TeamViewController(manager: Manager<in TeamViewWidget>, val position: Team
     override suspend fun createWidgetAndShow(settings: ExternalTeamViewSettings) {
         if (settings.teamId == null) {
             launchWhileWidgetShown {
-                DataBus.teamSpotlightFlow.await()
-                    .filter {
-                        val teamInfo = DataBus.currentContestInfo().teams[it.teamId]
-                        return@filter teamInfo?.isHidden?.not() ?: false
-                    }
-                    .collect {
-                        val staticSettings = settings.copy(teamId = it.teamId)
-                        super.createWidgetAndShow(staticSettings)
-                        delay(30.seconds)
-                    }
+                DataBus.teamSpotlightFlow.await().collect {
+                    val staticSettings = settings.copy(teamId = it.teamId)
+                    super.createWidgetAndShow(staticSettings)
+                    delay(30.seconds)
+                }
             }
         } else {
             super.createWidgetAndShow(settings)
