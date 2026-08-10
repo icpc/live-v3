8289710baa28bbd1b2445e2725c5dbf8f366ab01
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2026-08-10 08:38:43 +0200

    Hide new internal class from abi

--- src/cds/plugins/cms/src/main/kotlin/org/icpclive/cds/plugins/cms/model/Config.kt
+++ src/cds/plugins/cms/src/main/kotlin/org/icpclive/cds/plugins/cms/model/Config.kt
@@ -3,7 +3,7 @@ package org.icpclive.cds.plugins.cms.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-public class Config(
-    public val faces_extension: String,
-    public val flags_extension: String
+internal class Config(
+    val faces_extension: String,
+    val flags_extension: String
 )
